d42fa19428c9dd41951c7f5e6262be4f522a8468
Wilhelm Braun <wilhelmbraun@users.noreply.github.com>
2021-12-07 14:08:14 +0100

    Add files via upload
    Code to generate data for all parameter scans in the study.

--- code/parameter_scans.py
+++ code/parameter_scans.py
@@ -9,8 +9,8 @@ Created on Tue Dec 7 2021
 #############
 #############
 #This script generates data for Figures 4, 6 and 8. It uses the multiprocessing toolbox and either requires a cluster or a larger desktop computer (more than 10 threads) to finish in reasonable time.
-#Script is setup so that its execution will generate data for Fig. 8. It can easily be adapted (lines 800- 884) to generate the data for all parameter plots in the manuscript.
-#Code for model 1 starts on line 49, for model 2 on line 278 and code for model 3 on line 508.
+#Script is setup so that its execution will generate data for Fig. 8. It can easily be adapted (lines 1040- 1125) to generate the data for all parameter plots in the manuscript.
+#Code for model 1 starts on line 52, for model 1 with feedforward drive to basket cells on line 283, for model 2 on line 518 and code for model 3 on line 748.
 #############
 #############
 
@@ -278,6 +278,242 @@ def spike_generation_1(params):
     freq_max_e = freq_e[np.argmax(spec_e)]
 
     return [freq_max_i, freq_max_e, mean_spikes_I_nonsilent, mean_spikes_E_nonsilent, 1.-silent_percentage_of_I_population, 1.-silent_percentage_of_E_population]
+
+  
+#model 1 with feedforward excitatory drive to I cells
+def spike_generation_1_FFDrive_I(params):
+    param1, param2 = params[0], params[1]
+    n_e = param1
+    mean_amplitude = param2
+    
+    start_scope()
+    seed()
+    N_e = 12000 
+    N_i = 200 #1200 
+
+    #interneurons
+    E_rest_i = -65.*mV
+    E_I_i = -75.*mV
+    C_i = 100.0*pF
+    gl_i = 10.0*nS
+
+    #E population
+    E_e = 0.*mV
+    E_i = -68.*mV 
+    E_rest = -67.*mV
+    C = 275*pF
+    gl = 25.*nS
+
+    #synaptic parameters
+    #E-synapses
+    #"AMPA on pyramidal cells"
+    tau_r_E_e = 0.5*ms
+    tau_d_E_e = 1.8*ms
+    g_peak_E_e = 0.9*nS
+
+    #"GABA on CA1 pyramidal cells"
+    tau_r_E_i = 0.4*ms 
+    tau_d_E_i = 2.0*ms
+    g_peak_E_i = 9.0*nS 
+
+    #I-synapses
+    #"AMPA on interneurons"
+    tau_r_I_e = 0.5*ms 
+    tau_d_I_e = 1.2*ms 
+    g_peak_I_e = 3.0*nS
+
+    #"GABA on interneurons"
+    tau_r_I_i = 0.45*ms
+    tau_d_I_i = 1.2*ms
+    g_peak_I_i = 5.*nS
+
+    invpeak_I_e = (tau_r_I_e/tau_d_I_e) ** (tau_d_I_e/(tau_r_I_e-tau_d_I_e))
+    invpeak_I_i = (tau_r_I_i/tau_d_I_i) ** (tau_d_I_i/(tau_r_I_i-tau_d_I_i))
+
+    invpeak_E_e = (tau_r_E_e/tau_d_E_e) ** (tau_d_E_e/(tau_r_E_e-tau_d_E_e))
+    invpeak_E_i = (tau_r_E_i/tau_d_E_i) ** (tau_d_E_i/(tau_r_E_i-tau_d_E_i))
+
+    #conductances for the E cells
+    #AMPA
+    syn_eqs_E_e = Equations('''dg_e/dt = (invpeak_E_e*s_e - g_e)/tau_d_E_e : siemens
+                    ds_e/dt = -s_e/tau_r_E_e : siemens''')
+    #GABA
+    syn_eqs_E_i = Equations('''dg_i/dt = (invpeak_E_i*s_i - g_i)/tau_d_E_i : siemens
+                    ds_i/dt = -s_i/tau_r_E_i : siemens''')
+
+    #conductances for the I cells
+    #AMPA
+    syn_eqs_I_e = Equations('''dg_e/dt = (invpeak_I_e*s_e - g_e)/tau_d_I_e : siemens
+                    ds_e/dt = -s_e/tau_r_I_e : siemens''')
+    #GABA
+    syn_eqs_I_i = Equations('''dg_i/dt = (invpeak_I_i*s_i - g_i)/tau_d_I_i : siemens
+                    ds_i/dt = -s_i/tau_r_I_i : siemens''')
+
+
+    ####
+    peak_time_zero = 50.0
+    peak_time_zero_mean = 50.0
+    #intensities for sharp waves
+    #mean_intensity = 10.0
+    mean_intensity_i = 20.0 
+
+    #this time step is also used for the setup of g_chr and the simulation time step
+    time_step_array = 0.01 #ms, 0.01 is standard
+    defaultclock.dt = time_step_array*ms
+    ##time from 0 to 100 ms in steps of time_step_array ms
+    time_for_gaussian = np.arange(0.0, 100 + defaultclock.dt/ms, time_step_array)
+
+    #print time_for_gaussian
+
+    #value at t = 50 ms
+    temporal_sigma = 10.0 
+    normalization = 1./np.sqrt(2.*np.pi*temporal_sigma**(2))
+
+    #CV of amplitude distribution fixed
+    amplitude_CV = 0.5
+    
+
+    I_app_i = 0.0*nA
+    sigma_noise = 1.0*mV
+
+    eqs_e = (Equations('dV/dt = (gl*(E_rest - V) + (g_e + g_chr(t,i)*nS )*(E_e - V) + g_i*(E_i - V) +I_app_e)/C  + sigma_noise*sqrt(2/(C/gl))*xi: volt (unless refractory) \
+            I_app_e: ampere') + syn_eqs_E_e + syn_eqs_E_i)
+
+    group_e = NeuronGroup(N_e,  eqs_e, threshold='V>=-50*mV', reset='V=E_rest', refractory= 2.0*ms, method = 'euler')
+    #Sharp wave input to basket cells
+    eqs_i = (Equations('dV/dt = (gl_i*(E_rest_i - V) + (g_e + g_chr_i(t,i)*nS)*(E_e - V) + g_i*(E_I_i - V) + I_app_i)/C_i + sigma_noise*sqrt(2/(C_i/gl_i))*xi: volt (unless refractory)') + syn_eqs_I_e + syn_eqs_I_i)
+
+    group_i = NeuronGroup(N_i, eqs_i, threshold='V>=-52*mV', reset='V=E_rest_i', refractory= 1.0*ms, method = 'euler')
+
+
+    #synapses, e.g. from E to I
+    #kick sizes are g_peak
+    syn_ee = Synapses(group_e, group_e, on_pre='s_e += g_peak_E_e', delay = 1.0*ms)
+    syn_ii = Synapses(group_i, group_i, on_pre='s_i += g_peak_I_i', delay = 1.0*ms)
+
+    syn_ei = Synapses(group_e, group_i, on_pre='s_e += g_peak_I_e', delay = 1.0*ms)
+    syn_ie = Synapses(group_i, group_e, on_pre='s_i += g_peak_E_i', delay = 1.0*ms) #1.0*ms is standard
+
+    M_e = SpikeMonitor(group_e)
+    M_i = SpikeMonitor(group_i)
+    LFP_e = PopulationRateMonitor(group_e)
+    LFP_i = PopulationRateMonitor(group_i)
+
+    duration = 0.1*second
+
+    ###
+    mean_intensity = mean_amplitude
+    
+    #choose a subset of excitatory cells randomly
+    np.random.seed()
+    E_cells = np.arange(N_e)
+    shuffled_E_cells = np.random.permutation(E_cells)
+    #shuffled_E_cells = E_cells
+    shuffled_E_cells = shuffled_E_cells[0:int(n_e)]
+    #print shuffled_E_cells
+
+    active_neurons = np.zeros(N_e)
+    active_neurons[shuffled_E_cells] = 1.0
+    array_gaussian = []
+
+    numpy.random.seed()
+    #set up sharp wave input
+    amplitude = np.random.normal(mean_intensity, amplitude_CV*mean_intensity, N_e) #standard, used always
+    amplitude *= active_neurons
+    #amplitude[np.where(amplitude<=0)[0]] = 0 #not normally used
+    
+    #sharp wave input to basket cells
+    array_gaussian_i = []
+    #amplitude_i = np.random.normal(mean_intensity_i, amplitude_CV*mean_intensity_i, N_i)
+    amplitude_i = mean_intensity_i*np.ones(N_i) #used in December 2020 for control simulations
+
+    for time_index in np.arange(0, len(time_for_gaussian)):
+        random_activation = np.zeros(len(amplitude))
+        random_activation = (amplitude)*(1./normalization)*(gaussian_form(time_for_gaussian[time_index], temporal_sigma, peak_time_zero_mean)) #standard
+        array_gaussian.append(random_activation)
+        
+        #used in December 2020
+        random_activation_i = np.zeros(len(amplitude_i))
+        random_activation_i = (amplitude_i)*(1./normalization)*(gaussian_form(time_for_gaussian[time_index], temporal_sigma, peak_time_zero_mean))
+        array_gaussian_i.append(random_activation_i)
+
+    g_chr = TimedArray(array_gaussian, dt = time_step_array*ms)
+    g_chr_i = TimedArray(array_gaussian_i, dt = time_step_array*ms)
+    ###
+    
+    seed()
+    np.random.seed()
+    syn_ee.connect(p = 197./N_e)
+    syn_ii.connect(p = 0.2)
+    syn_ei.connect(p = 0.1)
+    syn_ie.connect(p = 0.1)
+    seed()
+    np.random.seed()
+    group_e.V = np.random.normal(E_rest/mV, 0.1, N_e)*mV
+    group_i.V = np.random.normal(E_rest_i/mV, 0.1, N_i)*mV
+    group_e.I_app_e = 0.0*nA
+    
+    run(duration)
+    #spike count computations, added 170719
+    spike_trains_E = M_e.spike_trains()
+    spike_trains_I = M_i.spike_trains()
+    number_of_spikes_E = np.zeros(N_e)
+    number_of_spikes_I = np.zeros(N_i)
+    delta_t = 50.0
+    t_min = peak_time_zero_mean - delta_t
+    t_max = peak_time_zero_mean + delta_t
+
+    spike_times_E = {}
+    for neuron in np.arange(N_e):
+        spike_times_neuron_E = spike_trains_E[neuron]/ms
+        #only count spikes in certain temporal range
+        spike_times_neuron_E = spike_times_neuron_E[np.where(spike_times_neuron_E>=t_min)[0]]
+        spike_times_neuron_E = spike_times_neuron_E[np.where(spike_times_neuron_E<=t_max)[0]]
+        #print(spike_times_neuron_E)
+        number_of_spikes_E[neuron] = len(spike_times_neuron_E[np.where(spike_times_neuron_E)[0]])
+        #spike_times_E[neuron] = spike_times_neuron_E
+
+        #print spike_trains_E[neuron]/ms, number_of_spikes_E[neuron]
+
+    spike_times_I = {}
+    for neuron in np.arange(N_i):
+        spike_times_neuron_I = spike_trains_I[neuron]/ms
+        spike_times_neuron_I = spike_times_neuron_I[np.where(spike_times_neuron_I>=t_min)[0]]
+        spike_times_neuron_I = spike_times_neuron_I[np.where(spike_times_neuron_I<=t_max)[0]]
+        number_of_spikes_I[neuron] = len(spike_times_neuron_I[np.where(spike_times_neuron_I)[0]])
+
+    #print number_of_spikes_E, number_of_spikes_I
+    #mean_spikes_E, mean_spikes_I = np.around(mean(number_of_spikes_E),1), np.around(mean(number_of_spikes_I),1)
+
+    mean_spikes_E_nonsilent, mean_spikes_I_nonsilent =  np.around(np.mean(number_of_spikes_E[np.where(number_of_spikes_E != 0)[0]]),1), np.around(np.mean(number_of_spikes_I[np.where(number_of_spikes_I != 0)[0]]),1)
+
+    silent_percentage_of_E_population =  len(np.where(number_of_spikes_E == 0)[0])/ np.float(N_e)
+    silent_percentage_of_I_population =  len(np.where(number_of_spikes_I == 0)[0])/ np.float(N_i)
+
+    ############
+    #spectrum computations
+
+    resolution_LFP = 0.2*ms #1.0*ms
+    population_rate_e = LFP_e.smooth_rate(window='gaussian', width=resolution_LFP)/Hz
+    population_rate_i = LFP_i.smooth_rate(window='gaussian', width=resolution_LFP)/Hz
+
+
+    t_min_rate = int(0/time_step_array)
+    t_max_rate = int(101/time_step_array)
+
+    time_rate_i = LFP_i.t/ms
+    sampling_frequency_i = 1000./(time_rate_i[1] - time_rate_i[0]) #Hz
+    factor_spec = 5.
+    freq_i, spec_i = signal.welch(population_rate_i[t_min_rate:t_max_rate], sampling_frequency_i, nperseg = len(time_rate_i[t_min_rate:t_max_rate])/factor_spec, nfft = 5*len(time_rate_i), window = 'hann', scaling = 'density')
+    freq_max_i = freq_i[np.argmax(spec_i)]
+    
+    time_rate_e = LFP_e.t/ms
+    sampling_frequency_e = 1000./(time_rate_e[1] - time_rate_e[0])
+    freq_e, spec_e = signal.welch(population_rate_e[t_min_rate:t_max_rate], sampling_frequency_e, nperseg = len(time_rate_e[t_min_rate:t_max_rate])/factor_spec, nfft = 5*len(time_rate_e), window = 'hann', scaling = 'density')
+    freq_max_e = freq_e[np.argmax(spec_e)]
+
+    return [freq_max_i, freq_max_e, mean_spikes_I_nonsilent, mean_spikes_E_nonsilent, 1.-silent_percentage_of_I_population, 1.-silent_percentage_of_E_population]
+
 
 #model 2 (Figs. 5 and 6)
 def spike_generation_2(params):
@@ -803,7 +1039,7 @@ def spike_generation_3_current(params):
 
 
 #######################################
-#model 1
+#model 1 with and without feedforward drive to I cells
 #######################################
 #number of excited E cells n_E
 #parameter_values_1 = np.arange(10, 1500, 30)
@@ -860,6 +1096,7 @@ for h, param1_in in  enumerate(parameter_values_1):
             result_list = []
             pool = multiprocessing.Pool(processes=n_threads)
             #result_list= pool.map(spike_generation_1, params_pool) #run scan for model 1
+            #result_list= pool.map(spike_generation_1_FFDrive_I, params_poo) #run scan for model 1 with feedforward drive to basket cells
             #result_list= pool.map(spike_generation_2, params_pool) #run scan for model 2
             result_list= pool.map(spike_generation_3_current, params_pool) #run scan for model 3
             pool.close()
